04204f6389d2cde9fc1f204087f566ae98bcb8da
Bartosz Kosmala <bartosz.kosmala@karhoo.com>
2023-02-16 16:56:32 +0100

    lint

--- command_test.go
+++ command_test.go
@@ -10,16 +10,20 @@ func TestCommand(t *testing.T) {
 	t.Parallel()
 
 	t.Run("provides output of an existing command", func(t *testing.T) {
+		t.Parallel()
+
 		cmd := *exec.Command("pwd")
 		got, err := run(cmd)
 
 		assertNoError(t, err)
-		if strings.Index(string(got), "gompose") < 0 {
+		if !strings.Contains(string(got), "gompose") {
 			t.Fatal("expected output to contain 'gompose', got", string(got))
 		}
 	})
 
 	t.Run("returns error when the command does not exist", func(t *testing.T) {
+		t.Parallel()
+
 		cmd := *exec.Command("this-shouldnt-work")
 		got, err := run(cmd)
 
